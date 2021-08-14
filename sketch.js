7adbc2ebde9fd60ee52569584d33312dfba4a451
AvaneeshGawde <74357818+AvaneeshGawde@users.noreply.github.com>
2021-08-14 18:56:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var database;
+var playerCount ;
+var gameState=0;
+var player,form,game;
+var allPlayers;
+var distance=0;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  
+  createCanvas(500,500);
+
+  game = new Game ()
+  game.getState();
+  game.start();
+
+  
+}
+
+function draw(){
+  if(playerCount===4){
+    game.update(1);
+
+
+  }
+  if(gameState===1){
+
+    clear();
+    game.play();
+
+  }
+  
+}
+
+
